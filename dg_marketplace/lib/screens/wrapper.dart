89b4e9d4f3bf46f7b5d73febc4a66a5a9bb8c7d7
Andreas Gilbert Leite <anlea016@osloskolen.no>
2025-01-30 09:03:58 +0100

    Authentication in the making

--- dg_marketplace/lib/screens/wrapper.dart
+++ dg_marketplace/lib/screens/wrapper.dart
@@ -1,11 +1,11 @@
+import 'package:dg_marketplace/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
-import 'package:dg_marketplace/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
   @override
   Widget build(BuildContext context) {
     //Return either home or authenticate widget
-    return home();
+    return Authenticate();
   }
 }
