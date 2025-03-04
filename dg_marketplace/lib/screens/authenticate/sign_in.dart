735e2b9dc180d7c40ed5a6ffb832b8622eda0874
Andreas Gilbert Leite <anlea016@osloskolen.no>
2025-03-04 10:24:49 +0100

    Sign up screen made

--- dg_marketplace/lib/screens/authenticate/sign_in.dart
+++ dg_marketplace/lib/screens/authenticate/sign_in.dart
@@ -1,1 +1,38 @@
+import 'package:dg_marketplace/screens/services/auth.dart';
+import 'package:flutter/material.dart';
 
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          title: const Text('Sign in')),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: ElevatedButton(
+            child: const Text('Sign in anon'),
+            onPressed: () async {
+              dynamic result = await _auth.signInAnon();
+              if (result == null) {
+                print('Error signing in');
+              } else {
+                print('Signed in');
+                print(result);
+              }
+            }),
+      ),
+    );
+  }
+}
--- dg_marketplace/lib/screens/services/auth.dart
+++ dg_marketplace/lib/screens/services/auth.dart
@@ -10,7 +10,10 @@ class AuthService {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
       return user;
-    } catch (e) {}
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 
   //sign in with email & password
