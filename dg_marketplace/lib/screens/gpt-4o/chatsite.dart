de948b877dcc6d67331caa54fc6587d5d22e404c
Andreas Gilbert Leite <anlea016@osloskolen.no>
2025-03-18 14:40:02 +0100

    Starting to add chatgpt bot to project

---
+++ dg_marketplace/lib/screens/gpt-4o/chatsite.dart
@@ -0,0 +1,11 @@
+import 'package:dg_marketplace/screens/gpt-4o/chat_page.dart';
+import 'package:flutter/material.dart';
+
+class ChatSite extends StatelessWidget {
+  const ChatSite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChatPage();
+  }
+}
