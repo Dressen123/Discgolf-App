fd46c713acc7baab8ad8f8eaa41f18c6e391b369
Andreas Gilbert Leite <anlea016@osloskolen.no>
2025-06-11 10:26:32 +0200

    changed backend location

--- dg_marketplace/lib/screens/discsites/discsite.dart
+++ dg_marketplace/lib/screens/discsites/discsite.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dg_marketplace/screens/discmodel/discmodel.dart'; // Import DiscService
-import 'package:dg_marketplace/screens/backend/database.dart';
+import 'package:dg_marketplace/screens/database/database.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
